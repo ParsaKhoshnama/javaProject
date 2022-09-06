b50451310131201d4bee5d98d252e1412b256270
parsa_khoshnama <parsa2001khoshnama@gmail.com>
2022-09-06 12:43:30 +0430

    request

---
+++ src/RemoveCommodityRequest.java
@@ -0,0 +1,19 @@
+public class RemoveCommodityRequest extends Request
+{
+    private PublicPropertiesOfGoods commodity;
+    RemoveCommodityRequest(Clerk clerk,PublicPropertiesOfGoods commodity,String context)
+    {
+        super(clerk,context);
+        this.commodity=commodity;
+        this.getClerk().getListOfCertainClerkRequests().add(this);
+        Admin.creatAdminObject().getRequestsOfclerks().add(this);
+    }
+    void setCommodiy(PublicPropertiesOfGoods commodity)
+    {
+        this.commodity=commodity;
+    }
+    PublicPropertiesOfGoods getCommodity()
+    {
+        return this.commodity;
+    }
+}
