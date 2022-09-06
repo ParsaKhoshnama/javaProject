8b1d38b261f0176c34a66a02211aeb3720a8ec1c
parsa_khoshnama <parsa2001khoshnama@gmail.com>
2022-09-06 12:33:29 +0430

    factor class

---
+++ src/BuyFactor.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+public class BuyFactor
+{
+    private String dateString;
+    private double price;
+    private double priceAfterDiscounts;
+    private ArrayList<Commodity> factorCommoditiesAl=new ArrayList<Commodity>();
+    private Buyer buyer;
+
+    BuyFactor(Buyer buyer,ArrayList<Commodity> factorCommoditiesAl,double price,double priceAfterDiscounts)
+    {
+        Date date = GregorianCalendar.getInstance().getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        this.dateString=sdf.format(date).toString();
+        this.price=price;
+        this.priceAfterDiscounts=priceAfterDiscounts;
+        this.factorCommoditiesAl=factorCommoditiesAl;
+    }
+    String getDateString()
+    {
+        return this.dateString;
+    }
+    double getPrice()
+    {
+        return this.price;
+    }
+    double getPriceAfterDiscounts()
+    {
+        return this.priceAfterDiscounts;
+    }
+    ArrayList<Commodity>getFactorCommoditiesAl()
+    {
+        return this.factorCommoditiesAl;
+    }
+}
