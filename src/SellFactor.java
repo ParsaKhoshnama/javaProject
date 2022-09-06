91bf01d6c87c60aba50e9583e23cd9a2db588a3f
parsa_khoshnama <parsa2001khoshnama@gmail.com>
2022-09-06 12:42:25 +0430

    sell factor

---
+++ src/SellFactor.java
@@ -0,0 +1,41 @@
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+public class SellFactor
+{
+    private Buyer buyer;
+    private String dateString;
+    private Commodity commodity;
+    private double price;
+    private double priceAfterDiscount;
+    SellFactor(Buyer buyer,Commodity commodity,double price,double priceAfterDiscount)
+    {
+        Date date = GregorianCalendar.getInstance().getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        this.dateString=sdf.format(date).toString();
+        this.buyer=buyer;
+        this.commodity=commodity;
+        this.price=price;
+        this.priceAfterDiscount=priceAfterDiscount;
+    }
+    double getPrice()
+    {
+        return this.price;
+    }
+    double getPriceAfterDiscount()
+    {
+        return this.priceAfterDiscount;
+    }
+    Buyer getBuyer()
+    {
+        return this.buyer;
+    }
+    Commodity getCommodity()
+    {
+        return this.commodity;
+    }
+    String getDateString()
+    {
+        return this.dateString;
+    }
+}
