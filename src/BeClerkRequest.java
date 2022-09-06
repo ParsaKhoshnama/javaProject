1f132380d922a13e1c90b2412eb686818aa32150
parsa_khoshnama <parsa2001khoshnama@gmail.com>
2022-09-06 12:31:30 +0430

    request

---
+++ src/BeClerkRequest.java
@@ -0,0 +1,12 @@
+
+public class BeClerkRequest extends Request
+{
+   // private boolean statusOfRequestforAdmin=false;//true=reject true=accept false=ignore
+  //  private boolean statusOfAddingRequestToREquestListOfAdmin;
+    BeClerkRequest(Clerk clerk,String context)
+    {
+
+        super(clerk,context);
+      //  this.getClerk().getListOfCertainClerkRequests().add(this);
+    }
+}
