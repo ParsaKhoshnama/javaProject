f894d9bd544ccf94eb4cc865a76d174c3f4ee62a
parsa_khoshnama <parsa2001khoshnama@gmail.com>
2022-09-06 12:54:39 +0430

    TV class

---
+++ src/Television.java
@@ -0,0 +1,192 @@
+import java.util.Scanner;
+
+public class Television extends HomeAppliance
+{
+    private  int sizeOfScreen;
+   private String gardeOfScreen;
+    Television(String name,String degreeName,boolean gaurantee,int sizeOfScreen,String gardeOfScreen,int count,String company,double price,String ID,String userName,String passWord,double percentOfDiscount,String statusForAdmin)
+    {
+        super(name,degreeName,gaurantee,company,price,ID,userName,passWord,percentOfDiscount,count);
+        this.sizeOfScreen=sizeOfScreen;
+        this.gardeOfScreen=gardeOfScreen;
+        if(statusForAdmin.equals("accept"))
+        {
+            this.setCount(super.findCountOfCertainHomeAppilaince(count));
+            this.clearHomeApplianceTelevision();
+            HomeAppliance.getListOfHomeAppliancesAl().add(this);
+        }
+        else
+            this.setCount(count);
+    }
+
+    void setSizeOfScreen(int sizeOfScreen)
+    {
+        this.sizeOfScreen=sizeOfScreen;
+    }
+    int getSizeOfScreen()
+    {
+        return this.sizeOfScreen;
+    }
+    void setGardeOfScreen(String gardeOfScreen)
+    {
+        this.gardeOfScreen=gardeOfScreen;
+    }
+    String getGardeOfScreen()
+    {
+        return this.gardeOfScreen;
+    }
+    private void clearHomeApplianceTelevision()
+    {
+        for (int i=0;i<HomeAppliance.getListOfHomeAppliancesAl().size();i++)
+        {
+            if(HomeAppliance.getListOfHomeAppliancesAl().get(i).getName().equals(this.getName()))
+            {
+                if(HomeAppliance.getListOfHomeAppliancesAl().get(i).getClerk().getPassWord().equals(this.getClerk().getPassWord()))
+                {
+                    if(HomeAppliance.getListOfHomeAppliancesAl().get(i) instanceof Television)
+                    {
+                        HomeAppliance.getListOfHomeAppliancesAl().remove(i);
+                    }
+                }
+            }
+        }
+    }
+    void creatTelevisionAterGettingAccept()
+    {
+
+        Television television=new Television(this.getName(),this.getDegreeName(),
+                this.isGuearantee(),this.getSizeOfScreen(),this.getGardeOfScreen(),
+                this.getCount(),this.getCompany(),this.getPrice(),this.getID(),
+                this.getClerk().getUserName(),this.getClerk().getPassWord(),this.getPercentOfDiscount(),"accept");
+        HomeAppliance.getListOfHomeAppliancesAl().add(television);
+        this.getClerk().getCommodityListOfCertainClerk().add(television);
+    }
+    void showTelevision()
+    {
+        System.out.println("Television: ");
+        System.out.println("name: "+this.getName());
+        System.out.println("degree name: "+this.getDegreeName());
+        System.out.println("ID: "+this.getID());
+        if(isGuearantee()==true)
+            System.out.println("has guarantee");
+        System.out.println("size of screen: "+this.getSizeOfScreen());
+        System.out.println("grade of screen: "+this.getGardeOfScreen());
+        System.out.println("company: "+this.getCompany());
+        System.out.println("count: "+this.getCount());
+        System.out.println("price: "+this.getPrice()+" T");
+        System.out.println("percent of dscount: "+this.getPercentOfDiscount());
+        System.out.println("price after discount: "+this.getPriceAfterDiscount()+" T");
+        System.out.println("average of scores: "+this.getAverageMark());
+    }
+    static void addTelevisionFunction(String name,String degreeOfconsumption,boolean gurant,String company,double price,double percentOfDiscount,String ID,String userName,String passWord)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.printf("enter size of screen: ");
+        int screenSize=sc.nextInt();
+        System.out.printf("enter count: ");
+        int count=sc.nextInt();
+        sc.nextLine();
+        System.out.printf("enter grade of screen: ");
+        String gradOfScreen=sc.nextLine();
+        Television television=new Television(name,degreeOfconsumption,gurant,screenSize,gradOfScreen,count,company,price,
+                ID,userName,passWord,percentOfDiscount,"ignore");
+        AddCommodityRequest request=new AddCommodityRequest(Clerk.findingClerk(userName,passWord),"add commodity request",television);
+    }
+    void changeTelevisionInformation(Clerk clerk)
+    {
+        Television television=new Television(this.getName(),this.getDegreeName(),
+                this.isGuearantee(),this.getSizeOfScreen(),this.getGardeOfScreen(),
+                this.getCount(),this.getCompany(),this.getPrice(),this.getID(),
+                this.getClerk().getUserName(),this.getClerk().getPassWord(),this.getPercentOfDiscount(),"for change Information request");
+        television.changeTelevisionField();
+        ChangeInformationOfCommodityRequest request=new ChangeInformationOfCommodityRequest(clerk,"Change information of commodity request",this,television);
+    }
+    private void changeTelevisionField()
+    {
+        String field;
+        Scanner sc=new Scanner(System.in);
+        while (true)
+        {
+            field=sc.nextLine();
+            if(field.equals("name"))
+            {
+                String name=sc.nextLine();
+                this.setName(name);
+            }
+            else if(field.equals("company"))
+            {
+                String company=sc.nextLine();
+                this.setCompany(company);
+            }
+            else if(field.equals("price"))
+            {
+                int price=sc.nextInt();
+                sc.nextLine();
+                this.setPrice(price);
+            }
+            else if(field.equals("percent of discount"))
+            {
+                int percentOfDiscount=sc.nextInt();
+                sc.nextLine();
+                this.setPercentOfDiscount(percentOfDiscount);
+            }
+            else if(field.equals("count"))
+            {
+                int count=sc.nextInt();
+                sc.nextLine();
+                this.setCount(count);
+            }
+            else if(field.equals("degree of concumption"))
+            {
+                String degreeOfconsumption;
+                while (true)
+                {
+                    System.out.printf("enter degree of energee cinsumption: ");
+                    degreeOfconsumption=sc.nextLine();
+                    if(degreeOfconsumption.equals("leave"))
+                        break;
+                    if(HomeAppliance.checkHomeApplianceDegreeOfenergyConsumption(degreeOfconsumption)==false)
+                        System.out.println("wrong name for energy consumption.try again");
+                    else
+                        break;
+                }
+            }
+            else if(field.equals("gurantee"))
+            {
+                String gurantee;
+                while (true)
+                {
+                    System.out.printf("guarantee: ");
+
+                    gurantee = sc.nextLine();
+                    if(gurantee.equals("leave"))
+                        break;
+                    if (gurantee.equals("true")) {
+                        this.setGuearantee(true);
+                        break;
+                    } else if (gurantee.equals("false")) {
+                        this.setGuearantee(false);
+                        break;
+                    } else
+                        System.out.println("wrong gurantee.try again");
+
+                }
+            }
+            else if(field.equals("size of screen"))
+            {
+                int sizeOfScreen=sc.nextInt();
+                sc.nextLine();
+                this.setSizeOfScreen(sizeOfScreen);
+            }
+            else if(field.equals("grade of screen"))
+            {
+                String gradeOfScreen=sc.nextLine();
+                this.setGardeOfScreen(gradeOfScreen);
+            }
+            else if(field.equals("leave"))
+                break;
+            else
+                System.out.println("Wrong command");
+        }
+    }
+}
