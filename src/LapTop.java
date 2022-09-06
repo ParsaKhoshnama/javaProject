7ee2fc0738d94e0e1c0d1afd5cde3ffa032d69fa
parsa_khoshnama <parsa2001khoshnama@gmail.com>
2022-09-06 12:50:42 +0430

    laptop class

---
+++ src/LapTop.java
@@ -0,0 +1,166 @@
+import java.util.Scanner;
+public class LapTop extends DigitalCommodity
+{
+    private boolean gamingCPU;
+   LapTop(String name,int ram,int valencyOfMemory,String operatingSystem,int weight,String company,double price,String ID,String userName,String passWord,boolean gamingCPU,int count,double percentOfDiscount,String statusForAdmin)
+   {
+       super(name,ram,valencyOfMemory,operatingSystem,weight,company,price,ID,userName,passWord,percentOfDiscount,count);
+       this.gamingCPU=gamingCPU;
+       if(statusForAdmin.equals("accept"))
+       {
+
+               this.setCount(super.findCountOfCertainDigitalCommodity(count));
+               this.clearDigitalCommodityLapTopAl(name);
+               DigitalCommodity.getDigiritlaCommodityAL().add(this);
+       }
+       else
+           this.setCount(count);
+   }
+   boolean isGamingCPU()
+   {
+       return this.gamingCPU;
+   }
+   void setGamingCPU(boolean gamingCPU)
+   {
+       this.gamingCPU=gamingCPU;
+   }
+   private void clearDigitalCommodityLapTopAl(String name)
+   {
+       for(int i=0;i<DigitalCommodity.getDigiritlaCommodityAL().size();i++)
+       {
+           if(DigitalCommodity.getDigiritlaCommodityAL().get(i).getName().equals(this.getName()))
+           {
+               if(DigitalCommodity.getDigiritlaCommodityAL().get(i).getClerk().getPassWord().equals(this.getClerk().getPassWord()))
+               {
+                   if(DigitalCommodity.getDigiritlaCommodityAL().get(i) instanceof LapTop)
+                   {
+                       DigitalCommodity.getDigiritlaCommodityAL().remove(i);
+                   }
+               }
+           }
+       }
+   }
+   void creatLaptopAfterGettingAccept()
+   {
+       LapTop lapTop=new LapTop(this.getName(),this.getRam(),
+               this.getValencyOfMemory(),this.getOperatingSystem(),this.getWeight(),this.getCompany(),
+               this.getPrice(),this.getID(),this.
+               getClerk().getUserName(),this.getClerk().getPassWord(),this.
+               isGamingCPU(),this.getCount(),this.getPercentOfDiscount(),"accept");
+       DigitalCommodity.getDigiritlaCommodityAL().add(lapTop);
+       this.getClerk().getCommodityListOfCertainClerk().add(lapTop);
+   }
+   void showLapTopInformation()
+   {
+       System.out.println("Lap Top: ");
+       System.out.println("name: "+this.getName());
+       System.out.println("ram: "+this.getRam()+"GB");
+       System.out.println("valnacy of memory: "+this.getValencyOfMemory()+"GB");
+       System.out.println("operating system: "+this.getOperatingSystem());
+       System.out.println("weight: "+this.getWeight()+"Kg");
+       System.out.println("company: "+this.getCompany());
+       System.out.println("price: "+this.getPrice()+" T");
+       System.out.println("ID: "+this.getID());
+       if(this.gamingCPU==true)
+           System.out.println("Gaming lapTop");
+       System.out.println("count: "+this.getCount());
+       System.out.println("percent of dscount: "+this.getPercentOfDiscount());
+       System.out.println("price after discount: "+this.getPriceAfterDiscount()+" T");
+       System.out.println("average of scores: "+this.getAverageMark());
+   }
+   static void addLapTopFunction(String name,String company,double price,double percentOfDiscount,String ID,String userName,String passWord,int ram,int valencyOfMemory,int weight,String operatingSystem)
+   {
+       Scanner sc=new Scanner(System.in);
+       System.out.printf("enter count: ");
+       int count=sc.nextInt();
+       sc.nextLine();
+       while (true)
+       {
+           System.out.printf("gamingCPU: ");
+           String gamingCPU=sc.nextLine();
+           if(gamingCPU.equals("true"))
+           {
+               LapTop lapTop=new LapTop(name,ram,valencyOfMemory,operatingSystem,weight,company,price,ID,userName,passWord,true,count,percentOfDiscount,"ignore");
+               AddCommodityRequest request=new AddCommodityRequest(Clerk.findingClerk(userName,passWord),"add commodity request",lapTop);
+               break;
+           }
+           else if(gamingCPU.equals("false"))
+           {
+               LapTop lapTop=new LapTop(name,ram,valencyOfMemory,operatingSystem,weight,company,price,ID,userName,passWord,false,count,percentOfDiscount,"ignore");
+               AddCommodityRequest request=new AddCommodityRequest(Clerk.findingClerk(userName,passWord),"add commodity request",lapTop);
+               break;
+           }
+           else
+               System.out.println("wrong.try again");
+       }
+   }
+   void changeInformationOfLapTop(Clerk clerk)
+   {
+       LapTop lapTop=new LapTop(this.getName(),this.getRam(),
+               this.getValencyOfMemory(),this.getOperatingSystem(),this.getWeight(),this.getCompany(),
+               this.getPrice(),this.getID(),this.
+               getClerk().getUserName(),this.getClerk().getPassWord(),this.
+               isGamingCPU(),this.getCount(),this.getPercentOfDiscount(),"for change Information request");
+       lapTop.changeFiledOfLapTop();
+       ChangeInformationOfCommodityRequest request=new ChangeInformationOfCommodityRequest(clerk,"Change information of commodity request",this,lapTop);
+
+   }
+   private void changeFiledOfLapTop()
+   {
+       String field;
+       Scanner sc=new Scanner(System.in);
+       while (true)
+       {
+           field=sc.nextLine();
+           if(field.equals("name"))
+           {
+               String name=sc.nextLine();
+               this.setName(name);
+           }
+           else if(field.equals("company"))
+           {
+               String company=sc.nextLine();
+               this.setCompany(company);
+           }
+           else if(field.equals("price"))
+           {
+               int price=sc.nextInt();
+               sc.nextLine();
+               this.setPrice(price);
+           }
+           else if(field.equals("percent of discount"))
+           {
+               int percentOfDiscount=sc.nextInt();
+               sc.nextLine();
+               this.setPercentOfDiscount(percentOfDiscount);
+           }
+           else if(field.equals("count"))
+           {
+               int count=sc.nextInt();
+               sc.nextLine();
+               this.setCount(count);
+           }
+           else if(field.equals("ram"))
+           {
+               int ram=sc.nextInt();
+               sc.nextLine();
+               this.setRam(ram);
+           }
+           else if(field.equals("valancy of memory"))
+           {
+               int valancyOfMemory=sc.nextInt();
+               sc.nextLine();
+               this.setValencyOfMemory(valancyOfMemory);
+           }
+           else if(field.equals("operating system"))
+           {
+               String operatingSystem=sc.nextLine();
+               this.setOperatingSystem(operatingSystem);
+           }
+           else if(field.equals("leave"))
+               break;
+           else
+               System.out.println("Wrong command");
+       }
+   }
+}
