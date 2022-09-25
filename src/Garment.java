29f06034d5adecb31cbcde652941d101128fd891
parsa_khoshnama <parsa2001khoshnama@gmail.com>
2022-09-25 09:08:53 +0330

    modify

--- src/Garment.java
+++ src/Garment.java
@@ -1,16 +1,69 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
-public abstract class Garment extends PublicPropertiesOfGoods
+public abstract class Garment extends PublicPropertiesOfGoods implements Comparable
 {
     private String country;
     private String genus;
     private static ArrayList<Garment> listOfAllGarmentsAl=new ArrayList<Garment>();
-    Garment(String name,String country, String genus,String company,double price,String ID,String username,String password,double percentOfDiscount,int count)
+    Garment(String name,String country, String genus,String company,double price,String ID,String username,String password,Discount discount,int count)
     {
-        super(name,company,price,ID,username,password,percentOfDiscount,count);
+        super(name,company,price,ID,username,password,discount,count);
         this.country=country;
         this.genus=genus;
+    }
+    public int compareTo(Object object)
+    {
+        if(this instanceof Dress && (object instanceof Dress)==false)
+            return -1;
+        else if((this instanceof Dress)==false && object instanceof Dress)
+            return 1;
+        else
+        {
+            if(this instanceof Dress && object instanceof Dress)
+            {
+                Dress dress=(Dress)object;
+                return this.dressCompareTo(dress);
+            }
+            else
+            {
+                Shoes shoes=(Shoes)object;
+                return this.shoesCompareTo(shoes);
+            }
+        }
+    }
+    private int dressCompareTo(Dress dress)
+    {
+        if(this.getName().compareTo(dress.getName())>0)
+            return -1;
+        else if(this.getName().compareTo(dress.getName())<0)
+            return 1;
+        else
+        {
+            if(((Dress)this).getSize()>dress.getSize())
+                return -1;
+            else if(((Dress)this).getSize()<dress.getSize())
+                return 1;
+            else
+                return this.compareToForGoods(dress);
+        }
+    }
+    private int shoesCompareTo(Shoes shoes)
+    {
+        if(this.getName().compareTo(shoes.getName())<0)
+            return 1;
+        else if(this.getName().compareTo(shoes.getName())>0)
+            return -1;
+        else
+        {
+            if(((Shoes)this).getSize()>shoes.getSize())
+                return -1;
+            else if(((Shoes)this).getSize()<shoes.getSize())
+                return 1;
+            else
+                return this.compareToForGoods(shoes);
+        }
     }
     String getCountry()
     {
@@ -86,8 +139,9 @@ public abstract class Garment extends PublicPropertiesOfGoods
                 }
             }
         }
+        Collections.sort(Garment.getListOfAllGarmentsAl());
     }
-    static void addGarmentFunction(String userName,String passWord,String commodityCommand,String company,double price,double percentOfDiscount,String ID,String name )
+    static void addGarmentFunction(String userName,String passWord,String commodityCommand,String company,double price,Discount discount,String ID,String name )
     {
         Scanner sc=new Scanner(System.in);
         System.out.printf("enter country: ");
@@ -96,11 +150,11 @@ public abstract class Garment extends PublicPropertiesOfGoods
         String genus=sc.nextLine();
         if(commodityCommand.equals("dress"))
         {
-            Dress.addDressFunction(name,country,genus,company,price,percentOfDiscount,ID,userName,passWord);
+            Dress.addDressFunction(name,country,genus,company,price,discount,ID,userName,passWord);
         }
         else
         {
-            Shoes.addShooesFunction(name,country,genus,company,price,percentOfDiscount,ID,userName,passWord);
+            Shoes.addShooesFunction(name,country,genus,company,price,discount,ID,userName,passWord);
         }
     }
 
